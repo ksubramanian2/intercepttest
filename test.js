237bdf3d99d9368c39fc6d791ecdd976f8c5e68f
Krishnamurti Subramanian <krishnamurti_subramanian@@intuit.com>
2022-11-05 17:14:58 +0530

    reproduce getRequests() call hanging

--- test.js
+++ test.js
@@ -4,13 +4,17 @@ it('getRequests test', async function() {
     this.timeout(30000);
     await browser.url('https://www.google.com');       
     await browser.setupInterceptor();
+
     //  Seach text entry
     const searchTextElement = await browser.$('input.gLFyf.gsfi');
     await searchTextElement.waitForDisplayed();
     await searchTextElement.waitForEnabled();
     await searchTextElement.setValue('abc');
 
-    await browser.pause(2000);
+    await browser.pause(5000);
+    // I have isolated the problem to the above browser.pause()
+    // If I comment it, getRequests() returns but gives a zero count for the number of requests
+    // If I uncomment it, getRequests() hangs
     console.log('entering getRequests')
     const requests = await browser.getRequests();
     console.log('returned from getRequests');
