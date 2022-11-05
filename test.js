ce042b4b004a82b5873d96019cd16ef07c3c976f
Krishnamurti Subramanian <krishnamurti_subramanian@@intuit.com>
2022-11-05 14:38:16 +0530

    fix: reproduce getREquests hang situation

--- test.js
+++ test.js
@@ -1,34 +1,23 @@
+const { assert } = require('chai');
+
 describe('reproduce getRequests hanging', function integrationTest() {
-    /*
     before(async () => {
-        
-        // await browser.url('https://localhost:9000');
-    });
-    */
-    it('getRequests test', async function() {
         await browser.url('https://www.google.com');
+    });
+
+    let pageLoadSuccess = null;
+      
+    it('getRequests test', async function() {
+        pageLoadSuccess = await browser.$('img.lnXdpd');
+        assert(pageLoadSuccess, 'The page did not load');
+       
         await browser.setupInterceptor();
-        // Dismiss sign in dialog 
-        /*
-        const signInDismiss = await browser.$(function() {return this.document.querySelector('button[data-dismiss="n"]');});
-        console.log(await signInDismiss.getAttribute('aria-label'));
-        await signInDismiss.waitForClickable();
-        await signInDismiss.click();
-        */
         //  Seach text entry
         const searchTextElement = await browser.$('input.gLFyf.gsfi');
         await searchTextElement.waitForDisplayed();
         await searchTextElement.waitForEnabled();
         await searchTextElement.setValue('abc');
-        /*
-        // Language selection
-        const selectLanguage = await browser.$('a[href*="wRky6sr0Sh7HBG7UfnGJkKZb3Ws"]');
-        console.log("selected language");
-        await selectLanguage.waitForClickable();
-        console.log("language ready to be clicked");
-        await selectLanguage.click();
-        console.log("clicked on selected language");
-        */
+
         await browser.pause(2000);
         console.log('entering getRequests')
         const requests = await browser.getRequests();
