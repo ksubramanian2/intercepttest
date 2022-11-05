a632e5e3c14c26654620a090d21afc12fe7c6013
Krishnamurti Subramanian <krishnamurti_subramanian@@intuit.com>
2022-11-05 13:44:33 +0530

    fix: enter search text

--- test.js
+++ test.js
@@ -1,22 +1,35 @@
 describe('reproduce getRequests hanging', function integrationTest() {
+    /*
     before(async () => {
-        await browser.url('https://www.google.com');
+        
         // await browser.url('https://localhost:9000');
     });
+    */
     it('getRequests test', async function() {
+        await browser.url('https://www.google.com');
         await browser.setupInterceptor();
+        // Dismiss sign in dialog 
         /*
-        const signInDismiss = await browser.$('button[data-dismiss="n"]');
+        const signInDismiss = await browser.$(function() {return this.document.querySelector('button[data-dismiss="n"]');});
+        console.log(await signInDismiss.getAttribute('aria-label'));
         await signInDismiss.waitForClickable();
         await signInDismiss.click();
+        */
+        //  Seach text entry
         const searchTextElement = await browser.$('input.gLFyf.gsfi');
         await searchTextElement.waitForDisplayed();
         await searchTextElement.waitForEnabled();
         await searchTextElement.setValue('abc');
+        /*
+        // Language selection
+        const selectLanguage = await browser.$('a[href*="wRky6sr0Sh7HBG7UfnGJkKZb3Ws"]');
+        console.log("selected language");
+        await selectLanguage.waitForClickable();
+        console.log("language ready to be clicked");
+        await selectLanguage.click();
+        console.log("clicked on selected language");
         */
-        const selectTamil = await browser.$('=தமிழ்');
-        await selectTamil.waitForClickable();
-        await selectTamil.click();
+        await browser.pause(2000);
         console.log('entering getRequests')
         const requests = await browser.getRequests();
         console.log('returned from getRequests');
