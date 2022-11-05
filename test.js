c537daa30c6741ed136de25a114d10994535eb9c
Krishnamurti Subramanian <krishnamurti_subramanian@@intuit.com>
2022-11-05 15:31:37 +0530

    reproduce getRequests() call hanging

--- test.js
+++ test.js
@@ -1,21 +1,18 @@
 const { assert } = require('chai');
 
-describe('reproduce getRequests hanging', function integrationTest() {    
+it('getRequests test', async function() {
     this.timeout(30000);
-    it('getRequests test', async function() {
-        this.timeout(30000);
-        await browser.url('https://www.google.com');       
-        await browser.setupInterceptor();
-        //  Seach text entry
-        const searchTextElement = await browser.$('input.gLFyf.gsfi');
-        await searchTextElement.waitForDisplayed();
-        await searchTextElement.waitForEnabled();
-        await searchTextElement.setValue('abc');
+    await browser.url('https://www.google.com');       
+    await browser.setupInterceptor();
+    //  Seach text entry
+    const searchTextElement = await browser.$('input.gLFyf.gsfi');
+    await searchTextElement.waitForDisplayed();
+    await searchTextElement.waitForEnabled();
+    await searchTextElement.setValue('abc');
 
-        await browser.pause(2000);
-        console.log('entering getRequests')
-        const requests = await browser.getRequests();
-        console.log('returned from getRequests');
-        console.log(`requests count = ${requests.length}`);
-    });
+    await browser.pause(2000);
+    console.log('entering getRequests')
+    const requests = await browser.getRequests();
+    console.log('returned from getRequests');
+    console.log(`requests count = ${requests.length}`);
 });
